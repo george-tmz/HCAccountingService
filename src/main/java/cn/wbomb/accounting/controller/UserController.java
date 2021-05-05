2ce081afd0b44c67366c6d5eab1db55fe5c60fc4
George <jobsteng@163.com>
2021-05-05 23:58:01 +0800

    feat

--- src/main/java/cn/wbomb/accounting/controller/UserController.java
+++ src/main/java/cn/wbomb/accounting/controller/UserController.java
@@ -1,11 +1,16 @@
 package cn.wbomb.accounting.controller;
 
 import cn.wbomb.accounting.converter.c2s.UserInfoC2SConverter;
+import cn.wbomb.accounting.exception.ErrorResponse;
+import cn.wbomb.accounting.exception.ResourceNotFoundException;
+import cn.wbomb.accounting.exception.ServiceException;
 import cn.wbomb.accounting.manager.UserInfoManager;
 import cn.wbomb.accounting.model.service.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +33,19 @@ public class UserController {
 
 
     @GetMapping("/{id}")
-    public UserInfo getUserInfoByUserId(@PathVariable("id") Long userId) {
+    public ResponseEntity<?> getUserInfoByUserId(@PathVariable("id") Long userId) {
         log.debug("Get user info by user id {}", userId);
-        val userInfo = userInfoManager.getUserInfoByUserId(userId);
-        return userInfoC2SConverter.convert(userInfo);
+        try {
+            val userInfo = userInfoManager.getUserInfoByUserId(userId);
+            return ResponseEntity.ok(userInfoC2SConverter.convert(userInfo));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ErrorResponse.builder()
+                            .code("USER_NOT_FOUND")
+                            .errorType(ServiceException.ErrorType.Client)
+                            .message(e.getMessage())
+                            .statusCode(HttpStatus.NOT_FOUND.value())
+                            .build());
+        }
     }
 }
